ad90cc0a6488dfda40c461ebbf56ca1490291083
Fransis96 <fransiskus.agustinus@students.utdi.ac.id>
2025-02-03 19:26:39 +0700

    Melakukan modifikasi

--- programqu/Main.java
+++ programqu/Main.java
@@ -1,5 +1,19 @@
 package programqu;
 
+class data{
+	private String nama;
+	private String nim;
+	private String jurusan;
+
+	public void setNama(String nama){ this.nama = nama; }
+	public void setNim(String nim){ this.nim = nim; }
+	public void setJurusan(String jurusan){ this.jurusan = jurusan; }
+
+	public String getNama(){ return nama; }
+	public String getNim(){ return nim; }
+	public String getJurusan(){ return jurusan; }
+}
+
 public class Main{
 
 	public static void main(String[] args){
